c70859c7b5e9d6fe1ca8f56a86fb22bf7c4d2b16
Renejay Flores <renejayflores@Renejays-MacBook-Air.local>
2026-06-06 20:33:57 +0800

    fix: whitelist Google reCAPTCHA in Helmet CSP policy
    CSP was blocking reCAPTCHA scripts and iframes:
    - Added google.com/recaptcha and gstatic.com/recaptcha to scriptSrc
    - Added frameSrc directive for reCAPTCHA iframe widget

--- src/middleware/security.js
+++ src/middleware/security.js
@@ -20,7 +20,8 @@ const helmetConfig = helmet({
       defaultSrc: ["'self'"],
       styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
       fontSrc: ["'self'", "https://fonts.gstatic.com"],
-      scriptSrc: ["'self'", "'unsafe-inline'", ...(isDev ? ["http://localhost:35729", "localhost:35729"] : [])],
+      scriptSrc: ["'self'", "'unsafe-inline'", "https://www.google.com/recaptcha/", "https://www.gstatic.com/recaptcha/", ...(isDev ? ["http://localhost:35729", "localhost:35729"] : [])],
+      frameSrc: ["'self'", "https://www.google.com/recaptcha/", "https://recaptcha.google.com/"],
       imgSrc: ["'self'", "data:", "https:"],
       connectSrc: ["'self'", ...(isDev ? ["ws://localhost:35729", "localhost:35729"] : [])]
     }
